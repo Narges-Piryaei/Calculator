f7c41e9f0fe2125e7bfcf2ff5116a5f0a0053607
Narges-Piryaei <nargespiryaei79@gmail.com>
2025-05-05 10:42:01 -0700

    افزودن فایل‌ها به برنچ develop

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-500 p-6 bg-blue-500">
+      سلام، Tailwind کار می‌کنه 🎉
+    </div>
+  );
+}
+
+
+export default App;
